1ea9b07c38f5a985e43b82bc3fa71a9dba8aa306
Matt Hays <mdhays1995@gmail.com>
2017-06-08 00:58:00 -0500

    Added home page routing and tested successfully.

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+
+
+function onSignIn(googleUser) {
+  const profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  goToHome();
+}
+
+function signOut() {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
+
+function goToHome() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "/home", true);
+  xhttp.send();
+}
--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.render('login');
 });
 
+app.get('/home', (req, res) => {
+  res.render('index');
+});
+
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
